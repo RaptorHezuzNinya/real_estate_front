12087cd93f71462984db6027308fd1cdbccde906
RaptorHezuzNinya <w22243vrh@gmail.com>
2018-09-04 15:02:05 +0200

    new filter Comp

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
+// import { withStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+// const styles = theme => ({
+// 	root: {
+// 		display: "flex",
+// 		flexWrap: "wrap"
+// 	},
+// 	formControl: {
+// 		margin: theme.spacing.unit,
+// 		minWidth: 120
+// 	},
+// 	selectEmpty: {
+// 		marginTop: theme.spacing.unit * 2
+// 	}
+// });
+
+class Filter extends Component {
+	state = {
+		age: "Hunderd",
+		name: "Month"
+	};
+
+	handleChange = event => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+	// className={classes.root}
+	// className={classes.formControl}
+	render() {
+		// const { classes } = this.props;
+		return (
+			<form autoComplete="on">
+				<FormControl>
+					<InputLabel>Month</InputLabel>
+					<Select
+						value={this.state.age}
+						onChange={this.handleChange}
+						inputProps={{
+							name: "Month",
+							id: "age-simple"
+						}}
+					>
+						<MenuItem value={10}>Ten</MenuItem>
+						<MenuItem value={20}>Twenty</MenuItem>
+						<MenuItem value={30}>Thirty</MenuItem>
+					</Select>
+				</FormControl>
+			</form>
+		);
+	}
+}
+
+// Filter.propTypes = {
+// 	classes: PropTypes.object.isRequired
+// };
+
+export default Filter;
